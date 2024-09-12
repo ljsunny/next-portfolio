619a711217f40c4cd05e853db54b21f40d8c6ecd
ljsunny <js961223@naver.com>
2024-09-12 13:20:52 -0700

    [add] add type animation

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,26 +1,46 @@
+'use client'
 import Image from "next/image";
 import React from "react";
+import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
   return (
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center mb-6 text-center sm:text-left">
-          <h1 className="text-white nb04 text-6xl font-extrabold sm:text-5xl">
-            Hello I'm Jisun
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl font-extrabold sm:text-5xl lg:text-6xl">
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-blue-500">
+              Hello I'm {" "}
+            </span>
+            <br/>
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'Jisun',
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                'Web developer',
+                1000,
+                'Software Engineer',
+                1000
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{ fontSize: '1.5em', display: 'inline-block' }}
+              repeat={Infinity}
+    />
           </h1>
-          <p className="text-[#ADB7BE] text-lg lg:text-xl ">
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl ">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat
             id dolore excepturi quasi dolorum blanditiis voluptates molestias in
             veritatis eos. Quos distinctio a eos incidunt reprehenderit possimus
             porro asperiores delectus?
           </p>
           <div>
-            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black sm:w-fit">Hire Me</button>
-            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 border border-white sm:w-fit">Download CV</button>
+            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-white w-full sm:w-fit mb-3 sm:mb-0 bg-gradient-to-br from-teal-400 via-blue-500 to-purple-500">Hire Me</button>
+            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 border border-white text-white w-full sm:w-fit">Download CV</button>
           </div>
         </div>
-        <div className="col-span-5">
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[500px] h-[400px] relative">
             <Image 
             src="/images/profile-photo.png"
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
-      <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-auto py-4">
+      <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-10 py-4">
         <HeroSection/>
       </main>
   );
