b8e7db63d9602360ab881df6af25fbe8c0359154
ljsunny <js961223@naver.com>
2024-09-12 14:30:25 -0700

    [new] add navbar

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -20,12 +20,12 @@ const HeroSection = () => {
                 1000, // wait 1s before replacing "Mice" with "Hamsters"
                 'Web developer',
                 1000,
-                'Software Engineer',
+                's',
                 1000
               ]}
               wrapper="span"
               speed={50}
-              style={{ fontSize: '1.5em', display: 'inline-block' }}
+              style={{ fontSize: '1.3em', display: 'inline-block' }}
               repeat={Infinity}
     />
           </h1>
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-      <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-10 py-4">
-        <HeroSection/>
+      <main className="flex min-h-screen flex-col bg-[#121212]">
+        <Navbar/>
+        <div className="container mx-auto px-12 py-4 mt-24">
+          <HeroSection/>
+        </div>
       </main>
   );
 }
