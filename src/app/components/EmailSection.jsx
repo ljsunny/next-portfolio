f02bee5eaa823234002115a879cf067178587e55
ljsunny <js961223@naver.com>
2024-09-23 11:18:40 -0700

    [fix] fix EmailSection

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -8,10 +8,11 @@ const EmailSection = () => {
         <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
-          I'm currently looking for new opportunities, my inbox is always open.
-          Whether you have a question or just want to say hi, I'll try my best
+          I&apos;m currently looking for new opportunities, my inbox is always open.
+          Whether you have a question or just want to say hi, I&apos;ll try my best
           to get back to you!
         </p>
+
         <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/ljsunny">
             <FaGithub size="30" color="#ffffff" />
